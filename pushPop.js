324f36f75dddc252ca9e08d08f773535f4c26db7
ferdousalam007 <iamferdousalam007@gmail.com>
2021-08-01 13:19:06 +0600

    push pop array

---
+++ pushPop.js
@@ -0,0 +1,12 @@
+var newNumbers = [20, 23, 25, 26, 30];
+//push method in array this method add element last serials
+var add = newNumbers.push(40);
+var add = newNumbers.push(50);
+var add = newNumbers.push(60);
+// console.log(add);
+console.log(newNumbers);
+//pop method in array this method remove element last serials
+var remove = newNumbers.pop();
+var remove = newNumbers.pop();
+// console.log(remove);
+console.log(newNumbers);
