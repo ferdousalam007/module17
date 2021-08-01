dfb71a69c93719a35433e946c32df678e330b202
ferdousalam007 <iamferdousalam007@gmail.com>
2021-08-01 13:10:51 +0600

    indexof

---
+++ array.js
@@ -0,0 +1,6 @@
+//create arrays
+var numbers = [1, 2, 3, 4, 5];
+var stringArray = ["ferdous", "alam", "Abdul Salam", "parag"];
+
+console.log(numbers);
+console.log(stringArray);
---
+++ arrayIndeOf.js
@@ -0,0 +1,6 @@
+var manyNumbers = [12, 15, 17, 20 , 22];
+var indexOfNum = manyNumbers.indexOf(22);
+var indexOfNum = manyNumbers.indexOf(22);
+var indexOfNum = manyNumbers.indexOf(22);
+var indexOfNum = manyNumbers.indexOf(12);
+console.log(indexOfNum);
