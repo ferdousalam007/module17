4f657d4886a52e01f582d7cc1bdafe55b7fc3364
ferdousalam007 <iamferdousalam007@gmail.com>
2021-08-01 12:44:50 +0600

    variables & data types

---
+++ variables.js
@@ -0,0 +1,15 @@
+//number
+var myNumber = 60;
+//string
+var myCourse = "programing hero"
+//boolean
+var trueReturn = true;
+var falseReturn = false;
+//integer
+var integer = [1, 2, 3, 4, 5];
+//floar
+var float = [1.12, 2.12, 3.15, 4.56];
+console.log(myNumber);
+console.log(myCourse);
+console.log(trueReturn);
+console.log(falseReturn);
