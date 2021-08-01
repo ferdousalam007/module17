8519cecca1a0f7c7337c8ac5ea9f0d70afd6c085
ferdousalam007 <iamferdousalam007@gmail.com>
2021-08-01 16:21:06 +0600

    all condition for eat

--- conditional.js
+++ conditional.js
@@ -1,1 +1,27 @@
-console.log(1)
+// if else condition
+// examples
+var foodReady = true;
+var chickenFryPrice = 99;
+var chickenRoastPrice = 199;
+var chickenCurryPrice = 299;
+var my_budget = 55;
+//ex -01
+
+// if(foodReady==true){
+//  console.log("ok serve me food");
+// }
+
+//ex-02
+if(foodReady==true){
+    if(chickenCurryPrice<=my_budget){
+        console.log("give me chicken curry");
+    }else if(chickenRoastPrice<=my_budget){
+        console.log("give me chicken roast");
+      }
+      else if(chickenFryPrice<=my_budget){
+          console.log("give me chicken fry");
+      
+      }else{
+        console.log("khabo na");
+    }
+}
